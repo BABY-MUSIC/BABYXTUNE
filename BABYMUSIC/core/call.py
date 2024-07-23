d5dea01682821de1025d29dae5b699e2ba65a4f0
BABY-MUSIC <tridevk442@gmail.com>
2024-07-23 12:02:14 +0530

    Update call.py

--- BABYMUSIC/core/call.py
+++ BABYMUSIC/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from RAUSHAN import LOGGER, YouTube, app
-from RAUSHAN.misc import db
-from RAUSHAN.utils.database import (
+from BABYMUSIC import LOGGER, YouTube, app
+from BABYMUSIC.misc import db
+from BABYMUSIC.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
@@ -31,11 +31,11 @@ from RAUSHAN.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from RAUSHAN.utils.exceptions import AssistantErr
-from RAUSHAN.utils.formatters import check_duration, seconds_to_min, speed_converter
-from RAUSHAN.utils.inline.play import stream_markup
-from RAUSHAN.utils.stream.autoclear import auto_clean
-from RAUSHAN.utils.thumbnails import get_thumb
+from BABYMUSIC.utils.exceptions import AssistantErr
+from BABYMUSIC.utils.formatters import check_duration, seconds_to_min, speed_converter
+from BABYMUSIC.utils.inline.play import stream_markup
+from BABYMUSIC.utils.stream.autoclear import auto_clean
+from BABYMUSIC.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
