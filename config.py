cf3904e0bd47a4650de57b1209e176d494e778de
BABY-MUSIC <tridevk442@gmail.com>
2024-07-25 12:12:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,18 +81,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/67f597d68f9b19ab02eec.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/9acd828ec45a363add2e9.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/c53d9a7df9dbaa5f4db05.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/6d0cc0300152510dfd999.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/6d0cc0300152510dfd999.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/47fbfa19bce7817625ad9.jpg"
 
 
 def time_to_seconds(time):
